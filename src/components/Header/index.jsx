e03ed823961707d2d113a37edb076d53a66c66dc
dhanraj <dhanraj@aequalisys.com>
2021-11-11 06:52:17 +0530

    Initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+const Header = () => {
+  return (
+    <div className="headerContainer">
+      <div className="logo">React-Chart</div>
+    </div>
+  );
+};
+
+export default Header;
