df82ba5fbd8f4eb3fc8870eb5748c7072042e8dd
dhanraj <dhanraj@aequalisys.com>
2021-11-11 07:29:41 +0530

    Line chart integration

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.scss";
+import LineChart from "./LineChart";
+
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="detailsContainer">
+        <h1>Line Chart</h1>
+      </div>
+      <div className="chartContainer">
+        <LineChart />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
